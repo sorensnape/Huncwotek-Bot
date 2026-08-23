2cbb5d66f4cf78e7a6ab3a89bc14964fc5af76b2
sorensnape <sorensnape@gmail.com>
2026-08-23 21:28:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,11 +44,11 @@ async def uratujprof(ctx):
 
 @bot.command()
 async def labirynt(ctx):
-    await ctx.send("*[Podbiega szybko i staje obok, wręczając Ci po cichu kawałek [pergaminu](https://jpcdn.it/img/small/d5953c28c807883d4823234cf4754814.webp)].* **Oto dzisiejszy labirynt! Nie zapomnij tylko odesłać wypełnionego poprzez formularz!** *[Podaje mu następny [pergamin](https://tiny.pl/dx98w) i zadowolony wraca do swojej wcześniejszej zabawy.]*")
+    await ctx.send("*[Podbiega szybko i staje obok, wręczając Ci po cichu kawałek [pergaminu](https://jpcdn.it/img/small/82c19bd4bf315d86c477759763b4ba6a.webp)].* **Oto dzisiejszy labirynt! Nie zapomnij tylko odesłać wypełnionego poprzez formularz!** *[Podaje mu następny [pergamin](https://tiny.pl/dx98w) i zadowolony wraca do swojej wcześniejszej zabawy.]*")
 
 @bot.command()
 async def zagadka(ctx):
-    await ctx.send("*[Podbiega szybko i staje przed tobą, kaszląc dwa razy.]* ***Czekasz na zagadkę? No dobrze… Oto dzisiejsza!***\n`Nie pokazuje ono tego, co jest w rzeczywistości, ale to, czego najbardziej pragniemy w głębi serca. Harry widział w nim swoich rodziców, a Dumbledore parę wełnianych skarpetek. Co to za przedmiot?`\n***Nie zapomnij o odpowiedzi! Masz tu jeszcze formularz na to.*** *[Podaje szybko części [pergaminu](https://tiny.pl/dxp66)].*")
+    await ctx.send("*[Podbiega szybko i staje przed tobą, kaszląc dwa razy.]* ***Czekasz na zagadkę? No dobrze… Oto dzisiejsza!***\n`To magiczne miejsce ukryte przed oczami mugoli, gdzie młodzi czarodzieje uczą się rzucać zaklęcia i warzyć eliksiry. Prowadzi do niego ukryty peron na stacji kolejowej. Jak nazywa się ta szkoła?`\n***Nie zapomnij o odpowiedzi! Masz tu jeszcze formularz na to.*** *[Podaje szybko części [pergaminu](https://tiny.pl/dxp66)].*")
 
 # Uruchomienie "budzika"
 keep_alive()
