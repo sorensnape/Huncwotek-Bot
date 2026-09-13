516c1da4596e8837f8c46142c1688ee862d8a349
sorensnape <sorensnape@gmail.com>
2026-09-13 20:45:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,11 +44,11 @@ async def uratujprof(ctx):
 
 @bot.command()
 async def labirynt(ctx):
-    await ctx.send("*[Podbiega szybko i staje obok, wręczając Ci po cichu kawałek [pergaminu](https://jpcdn.it/img/small/0257f36212dfda8b8060cfd9d6464de3.webp)].* **Oto dzisiejszy labirynt! Nie zapomnij tylko odesłać wypełnionego poprzez formularz!** *[Podaje mu następny [pergamin](https://tiny.pl/dx98w) i zadowolony wraca do swojej wcześniejszej zabawy.]*")
+    await ctx.send("*[Podbiega szybko i staje obok, wręczając Ci po cichu kawałek [pergaminu](https://jpcdn.it/img/small/7206378f13b45dd74dae9d5003f4feb2.webp)].* **Oto dzisiejszy labirynt! Nie zapomnij tylko odesłać wypełnionego poprzez formularz!** *[Podaje mu następny [pergamin](https://tiny.pl/dx98w) i zadowolony wraca do swojej wcześniejszej zabawy.]*")
 
 @bot.command()
 async def zagadka(ctx):
-    await ctx.send("*[Podbiega szybko i staje przed tobą, kaszląc dwa razy.]* ***Czekasz na zagadkę? No dobrze… Oto dzisiejsza!***\n`Zawsze nosi kolorowe szaty, uwielbia słodycze i jest uważany za najpotężniejszego czarodzieja swoich czasów. To właśnie on przez lata kierował Hogwartem i opiekował się Harrym. Kto to taki?`\n***Nie zapomnij o odpowiedzi! Masz tu jeszcze formularz na to.*** *[Podaje szybko części [pergaminu](https://tiny.pl/dxp66)].*")
+    await ctx.send("*[Podbiega szybko i staje przed tobą, kaszląc dwa razy.]* ***Czekasz na zagadkę? No dobrze… Oto dzisiejsza!***\n`Są wykonane z drewna i mają różne parametry prędkości oraz zwrotności. Każdy szukający musi posiadać jedną z nich, aby móc brać udział w meczach w powietrzu. Czym podróżują czarodzieje?`\n***Nie zapomnij o odpowiedzi! Masz tu jeszcze formularz na to.*** *[Podaje szybko części [pergaminu](https://tiny.pl/dxp66)].*")
 
 # Uruchomienie "budzika"
 keep_alive()
